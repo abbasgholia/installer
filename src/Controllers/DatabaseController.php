721f1be9c4ae42fc7267f73f24a847ff487f6c47
abbasgholia <abbasgholia@yahoo.com>
2025-02-13 00:15:36 +0330

    Update DatabaseController.php

--- src/Controllers/DatabaseController.php
+++ src/Controllers/DatabaseController.php
@@ -2,9 +2,11 @@
 
 namespace Froiden\LaravelInstaller\Controllers;
 
+use App\Models\Setting;
+use App\Models\SysSpc;
 use Illuminate\Routing\Controller;
 use Froiden\LaravelInstaller\Helpers\DatabaseManager;
-
+use App\Models\Year;
 class DatabaseController extends Controller
 {
 
@@ -31,8 +33,20 @@ class DatabaseController extends Controller
         // Sometimes migration file and seed files may take more then 30 seconds so we are going to set it 0 that indicates
         // that there is no time limit for execution.
 
+
         set_time_limit(0);
+
         $response = $this->databaseManager->migrateAndSeed();
+
+        Year::create(['year' => session('year') , "close"=>0]);
+        Setting::create(['name_spc' => "invable" , "value" => session('invable') , "year" => session('year') ]);
+        Setting::create(['name_spc' => "serializable" , "value" => session('serializable'), "year" => session('year')  ]);
+        Setting::create(['name_spc' => "batchable" , "value" => session('batchable') , "year" => session('year') ]);
+        Setting::create(['name_spc' => "barcode_type" , "value" => "QRCODE", "year" => session('year')  ]);
+        Setting::create(['name_spc' => "currently_counting_time" , "value" => 1 , "year" => session('year') ]);
+        SysSpc::create(['name_spc' => "name_co" , "value" => session('name_co') ]);
+
+
 
         return redirect()->route('LaravelInstaller::final')
                          ->with(['message' => $response]);
